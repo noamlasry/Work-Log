c7d90717aee60e5270c3059b6e8360bd154ef2d9
Arkanoid <noamlasry02@gmail.com>
2020-01-04 10:34:33 +0200

    add insert and exit button

--- app/src/main/java/com/noamls_amirbs/worklog/MainActivity.java
+++ app/src/main/java/com/noamls_amirbs/worklog/MainActivity.java
@@ -3,12 +3,20 @@ package com.noamls_amirbs.worklog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button insertButton,exitButton;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/noamls_amirbs/worklog/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.noamls_amirbs.worklog;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by AbhiAndroid
+ */
+
+public class SplashActivity extends Activity {
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+}
