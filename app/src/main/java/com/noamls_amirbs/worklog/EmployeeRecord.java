bd7c1cb5210f58b1c53c53395c23c791f3158af0
Arkanoid <noamlasry02@gmail.com>
2020-01-04 11:39:54 +0200

    add intent to employee record

---
+++ app/src/main/java/com/noamls_amirbs/worklog/EmployeeRecord.java
@@ -0,0 +1,14 @@
+package com.noamls_amirbs.worklog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmployeeRecord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_record);
+    }
+}
